be7ac40478747761fabf7842c34493eda9fad5bf
ruan-alves-dev <ruanalvesoliveira630@gmail.com>
2025-08-18 20:53:19 -0300

    Create media_ponderada.java

---
+++ Ativdade_1/media_ponderada.java
@@ -0,0 +1,32 @@
+package scaner;
+import java.util.Scanner;
+// import java.lang.Math;
+
+public class area_circulo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double somaNotasPesos = 0;
+        double somaPesos = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Digite a nota " + i + ": ");
+            double nota = sc.nextDouble();
+
+            System.out.print("Digite o peso da nota " + i + ": ");
+            double peso = sc.nextDouble();
+
+            somaNotasPesos += nota * peso; 
+            somaPesos += peso;             
+        }
+
+        double mediaPonderada = somaNotasPesos / somaPesos;
+
+        System.out.println("A média ponderada é: " + mediaPonderada);
+
+        sc.close();
+
+	}
+
+}
